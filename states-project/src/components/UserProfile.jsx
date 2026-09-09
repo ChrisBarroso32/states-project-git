9da8063f590db0630fecdbd8b6bf547ecd2ca622
Christian <chrisbarroso32@gmail.com>
2026-09-09 13:47:00 -0600

    Se añadio el archivo README.md a la repo

--- states-project/src/components/UserProfile.jsx
+++ states-project/src/components/UserProfile.jsx
@@ -1,5 +1,5 @@
-import { useContext } from 'react' 
-import { UserContext } from '../contexts/user-context-provider.jsx';
+// import { useContext } from 'react' 
+// import { UserContext } from '../contexts/user-context-provider.jsx';
 import { useUserStore } from '../zustand/useUserStore.js';
 
 export const UserProfile = () => {
